c73663e22e01e7d66f160946fbe5abe914e3fee7
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-11-16 23:33:53 +0530

    create CustomerController class for the access all frontend side and backend sides request and response

--- point-of-sale/src/main/java/com/pos/springproject/point_of_sale/controller/CustomerController.java
+++ point-of-sale/src/main/java/com/pos/springproject/point_of_sale/controller/CustomerController.java
@@ -1,3 +1,21 @@
+package com.pos.springproject.point_of_sale.controller;
+
+import com.pos.springproject.point_of_sale.dto.CustomerDTO;
+import com.pos.springproject.point_of_sale.dto.request.CustomerSaveRequestDTO;
+import com.pos.springproject.point_of_sale.dto.request.CustomerUpdateRequestDTO;
+import com.pos.springproject.point_of_sale.service.CustomerService;
+import com.pos.springproject.point_of_sale.service.impl.CustomerServiceIMPL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/customer")
+@CrossOrigin
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
     @PostMapping(path = "/save")
     public String saveCustomer(@RequestBody CustomerSaveRequestDTO customerSaveRequestDTO){
         String id = customerService.addCustomer(customerSaveRequestDTO);
