43d03a5c330a6042808664371ad1f577a1c99650
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-11-16 23:41:43 +0530

    create interface CustomerService for doing functions in Controller

--- point-of-sale/src/main/java/com/pos/springproject/point_of_sale/service/CustomerService.java
+++ point-of-sale/src/main/java/com/pos/springproject/point_of_sale/service/CustomerService.java
@@ -1,4 +1,12 @@
 package com.pos.springproject.point_of_sale.service;
 
+import com.pos.springproject.point_of_sale.dto.CustomerDTO;
+import com.pos.springproject.point_of_sale.dto.request.CustomerSaveRequestDTO;
+import com.pos.springproject.point_of_sale.dto.request.CustomerUpdateRequestDTO;
+
 public interface CustomerService {
+
+    String addCustomer(CustomerSaveRequestDTO customerSaveRequestDTO);
+
+    String updateCustomer(CustomerUpdateRequestDTO customerUpdateRequestDTO);
 }
